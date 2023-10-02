ff41bc6268c2976b95144b7d24c88a765f932c13
Shenya De Silva <sde-silv@c4c3c5.42berlin.de>
2023-10-02 14:17:43 +0200

    Initial commit

---
+++ basics/ft_lstclear.c
@@ -0,0 +1,60 @@
+#include "liblist.h"
+
+void	del(void *ptr)
+{
+	if (ptr)
+	{
+        ptr = 0;
+	}
+}
+
+void    ft_lstclear(t_list **lst, void (*del)(void*))
+{
+    t_list  *current;
+
+    current = *lst;
+    while (current != NULL)
+    {
+        ft_delone(current, del);
+        current = current->next;
+    }
+}
+
+int main(void)
+{
+	t_list *head;
+	t_list *current;
+	t_list **lst;
+	t_list	*last;
+	int   size;
+
+
+	head = ft_lstnew((void *)20);
+    *lst = head;
+    head->next = ft_lstnew((void *)30);
+	ft_lstadd_front(lst, ft_lstnew((void *)10));
+	ft_lstadd_back(lst, ft_lstnew((void *)40));
+
+    current = *lst;
+    
+    size = ft_lstsize(*lst);
+    printf("size: %d\n", size);
+    while (current != NULL)
+    {
+    	printf("%zu\n", (size_t)current->content);
+        current = current->next;
+    }
+    last = ft_lstlast(*lst);
+    printf("last: %zu\n", (size_t)(last->content));
+    
+    current = *lst;
+    ft_lstclear(head, del);
+    
+    while (size>0)
+    {
+    	printf("%zu\n", (size_t)current->content);
+        current = current->next;
+        size --;
+    }
+	return (0);
+}
